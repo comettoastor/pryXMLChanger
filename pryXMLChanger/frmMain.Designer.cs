8917002ceb65a04cbee6b16ae0baa2b7c01afef6
Tkom <Tkom@GONZALO-PC>
2022-10-01 09:08:41 -0300

    Agregar archivos de proyecto.

---
+++ pryXMLChanger/frmMain.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace pryXMLChanger
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.btnDirectorioOriginal = new System.Windows.Forms.Button();
+            this.txtDirectorioOriginal = new System.Windows.Forms.TextBox();
+            this.lblDirectorioOriginal = new System.Windows.Forms.Label();
+            this.lblDirectorioModificado = new System.Windows.Forms.Label();
+            this.txtDirectorioModificado = new System.Windows.Forms.TextBox();
+            this.btnDirectorioModificado = new System.Windows.Forms.Button();
+            this.lblEstado = new System.Windows.Forms.Label();
+            this.btnDescargarOriginal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Enabled = false;
+            this.btnAplicar.Location = new System.Drawing.Point(294, 201);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(75, 22);
+            this.btnAplicar.TabIndex = 0;
+            this.btnAplicar.Text = "Apply";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            this.btnAplicar.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // lblPorcentaje
+            // 
+            this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.Enabled = false;
+            this.lblPorcentaje.Location = new System.Drawing.Point(12, 122);
+            this.lblPorcentaje.Name = "lblPorcentaje";
+            this.lblPorcentaje.Size = new System.Drawing.Size(106, 13);
+            this.lblPorcentaje.TabIndex = 1;
+            this.lblPorcentaje.Text = "Percentage to boost:";
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Enabled = false;
+            this.txtPorcentaje.Location = new System.Drawing.Point(12, 138);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(106, 20);
+            this.txtPorcentaje.TabIndex = 2;
+            this.txtPorcentaje.TextChanged += new System.EventHandler(this.txtPorcentaje_TextChanged);
+            this.txtPorcentaje.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPorcentaje_KeyPress);
+            // 
+            // btnDirectorioOriginal
+            // 
+            this.btnDirectorioOriginal.Location = new System.Drawing.Point(294, 37);
+            this.btnDirectorioOriginal.Name = "btnDirectorioOriginal";
+            this.btnDirectorioOriginal.Size = new System.Drawing.Size(75, 23);
+            this.btnDirectorioOriginal.TabIndex = 3;
+            this.btnDirectorioOriginal.Text = "Search";
+            this.btnDirectorioOriginal.UseVisualStyleBackColor = true;
+            this.btnDirectorioOriginal.Click += new System.EventHandler(this.btnDirectorio_Click);
+            // 
+            // txtDirectorioOriginal
+            // 
+            this.txtDirectorioOriginal.Location = new System.Drawing.Point(12, 39);
+            this.txtDirectorioOriginal.Name = "txtDirectorioOriginal";
+            this.txtDirectorioOriginal.Size = new System.Drawing.Size(276, 20);
+            this.txtDirectorioOriginal.TabIndex = 4;
+            this.txtDirectorioOriginal.TextChanged += new System.EventHandler(this.txtDirectorioOriginal_TextChanged);
+            // 
+            // lblDirectorioOriginal
+            // 
+            this.lblDirectorioOriginal.AutoSize = true;
+            this.lblDirectorioOriginal.Location = new System.Drawing.Point(12, 23);
+            this.lblDirectorioOriginal.Name = "lblDirectorioOriginal";
+            this.lblDirectorioOriginal.Size = new System.Drawing.Size(138, 13);
+            this.lblDirectorioOriginal.TabIndex = 5;
+            this.lblDirectorioOriginal.Text = "Select original types.xml file:";
+            // 
+            // lblDirectorioModificado
+            // 
+            this.lblDirectorioModificado.AutoSize = true;
+            this.lblDirectorioModificado.Enabled = false;
+            this.lblDirectorioModificado.Location = new System.Drawing.Point(12, 72);
+            this.lblDirectorioModificado.Name = "lblDirectorioModificado";
+            this.lblDirectorioModificado.Size = new System.Drawing.Size(210, 13);
+            this.lblDirectorioModificado.TabIndex = 8;
+            this.lblDirectorioModificado.Text = "Select folder for final modified types.xml file:";
+            // 
+            // txtDirectorioModificado
+            // 
+            this.txtDirectorioModificado.Enabled = false;
+            this.txtDirectorioModificado.Location = new System.Drawing.Point(12, 88);
+            this.txtDirectorioModificado.Name = "txtDirectorioModificado";
+            this.txtDirectorioModificado.Size = new System.Drawing.Size(276, 20);
+            this.txtDirectorioModificado.TabIndex = 7;
+            this.txtDirectorioModificado.TextChanged += new System.EventHandler(this.txtDirectorioModificado_TextChanged);
+            // 
+            // btnDirectorioModificado
+            // 
+            this.btnDirectorioModificado.Enabled = false;
+            this.btnDirectorioModificado.Location = new System.Drawing.Point(294, 86);
+            this.btnDirectorioModificado.Name = "btnDirectorioModificado";
+            this.btnDirectorioModificado.Size = new System.Drawing.Size(75, 23);
+            this.btnDirectorioModificado.TabIndex = 6;
+            this.btnDirectorioModificado.Text = "Search";
+            this.btnDirectorioModificado.UseVisualStyleBackColor = true;
+            this.btnDirectorioModificado.Click += new System.EventHandler(this.btnDirectorioModificado_Click);
+            // 
+            // lblEstado
+            // 
+            this.lblEstado.AutoSize = true;
+            this.lblEstado.Location = new System.Drawing.Point(80, 206);
+            this.lblEstado.Name = "lblEstado";
+            this.lblEstado.Size = new System.Drawing.Size(0, 13);
+            this.lblEstado.TabIndex = 9;
+            // 
+            // btnDescargarOriginal
+            // 
+            this.btnDescargarOriginal.Location = new System.Drawing.Point(274, 9);
+            this.btnDescargarOriginal.Name = "btnDescargarOriginal";
+            this.btnDescargarOriginal.Size = new System.Drawing.Size(113, 22);
+            this.btnDescargarOriginal.TabIndex = 11;
+            this.btnDescargarOriginal.Text = "Download Original";
+            this.btnDescargarOriginal.UseVisualStyleBackColor = true;
+            this.btnDescargarOriginal.Click += new System.EventHandler(this.btnDescargarOriginal_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 228);
+            this.Controls.Add(this.btnDescargarOriginal);
+            this.Controls.Add(this.lblEstado);
+            this.Controls.Add(this.lblDirectorioModificado);
+            this.Controls.Add(this.txtDirectorioModificado);
+            this.Controls.Add(this.btnDirectorioModificado);
+            this.Controls.Add(this.lblDirectorioOriginal);
+            this.Controls.Add(this.txtDirectorioOriginal);
+            this.Controls.Add(this.btnDirectorioOriginal);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.lblPorcentaje);
+            this.Controls.Add(this.btnAplicar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DayZ Loot Boost";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.Label lblPorcentaje;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.Button btnDirectorioOriginal;
+        private System.Windows.Forms.TextBox txtDirectorioOriginal;
+        private System.Windows.Forms.Label lblDirectorioOriginal;
+        private System.Windows.Forms.Label lblDirectorioModificado;
+        private System.Windows.Forms.TextBox txtDirectorioModificado;
+        private System.Windows.Forms.Button btnDirectorioModificado;
+        private System.Windows.Forms.Label lblEstado;
+        private System.Windows.Forms.Button btnDescargarOriginal;
+    }
+}
+
---
+++ pryXMLChanger/frmMain.cs
@@ -0,0 +1,238 @@
+﻿using pryXMLChanger.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace pryXMLChanger
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if (txtDirectorioOriginal.Text != "" && txtDirectorioModificado.Text != "")
+            {
+                //Extrae la dirección del directorio original y reemplaza las "\" que vienen por default por "\\" que utiliza C#
+                string strDirectorioOriginal = txtDirectorioOriginal.Text;
+                strDirectorioOriginal = strDirectorioOriginal.Replace(@"\", @"\\");
+
+                //Extrae la dirección del directorio final modificado y reemplaza las "\" que vienen por default por "\\" que utiliza C#
+                string strDirectorioModificado = txtDirectorioModificado.Text;
+                strDirectorioModificado = strDirectorioModificado.Replace(@"\", @"\\");
+
+                //Verifica que haya un porcentaje ingresado
+                if (txtPorcentaje.Text != "")
+                {
+                    MessageBox.Show("Process started it might take a while depending on your PC, press OK to continue!", "Started", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    //Carga del types.xml y creación de la lista de nodos
+                    XmlDocument docXML = new XmlDocument();
+
+                    docXML.Load(strDirectorioOriginal);
+
+                    XmlNodeList varListaNodosNominal = docXML.DocumentElement.SelectNodes("//nominal");
+
+                    //Extracción del porcentaje ingresado y conversión para multiplicacíón
+                    double varPorcentaje = Convert.ToDouble(txtPorcentaje.Text);
+                    varPorcentaje = varPorcentaje / 100 + 1;
+
+                    //Para cada nodo de la lista de nodos del types.xml aumenta el nominal de cada tipo de loot por el porcentaje ingresado
+                    foreach (XmlNode varNodoNominal in varListaNodosNominal)
+                    {
+                        double dobNominal = Convert.ToDouble(varNodoNominal.InnerText); //Variable double que almacena el nominal que posteriormente va a ser aumentado
+                        dobNominal = dobNominal * varPorcentaje; //Aumento del nominal por el porcentaje ingresado
+                        dobNominal = Math.Ceiling(dobNominal); //Redondeo del nominal hacia arriba
+                        string strNominalNuevo = Convert.ToString(dobNominal); //Asigno el valor del double dobNominal a una string llamada strNominalNuevo que literalmente va a ser el nuevo nominal aumentado
+                        varNodoNominal.InnerText = strNominalNuevo; //Reemplazao del nominal viejo con el aumentado
+                        docXML.Save(strDirectorioModificado + "\\types_modified_%" + txtPorcentaje.Text + ".xml"); //Guarda el nominal uno a la vez (¡REVISAR ESTO A LO MEJOR SE PUEDE HACER QUE SE GUARDE TODO JUNTO PORQUE EL PROGRAMA SE PAPEA AL GUARDAR TODO INDIVIDUAL!)
+                    }
+
+                    //Selección de nodos del nodo minimum
+                    XmlNodeList varListaNodosMin = docXML.DocumentElement.SelectNodes("//min");
+
+                    //Para cada nodo de la lista de nodos del types.xml aumenta el min de cada tipo de loot por el porcentaje ingresado
+                    foreach (XmlNode varNodoMin in varListaNodosMin)
+                    {
+                        double dobMin = Convert.ToDouble(varNodoMin.InnerText); //Variable double que almacena el min que posteriormente va a ser aumentado
+                        dobMin = dobMin * varPorcentaje; //Aumento del min por el porcentaje ingresado
+                        dobMin = Math.Ceiling(dobMin); //Redondeo del min hacia arriba
+                        string strMinNuevo = Convert.ToString(dobMin); //Asigno el valor del double dobMin a una string llamada strNominalNuevo que literalmente va a ser el nuevo nominal aumentado
+                        varNodoMin.InnerText = strMinNuevo; //Reemplazao del min viejo con el aumentado
+                        docXML.Save(strDirectorioModificado + "\\types_modified_%" + txtPorcentaje.Text + ".xml"); //Guarda el min uno a la vez (¡REVISAR ESTO A LO MEJOR SE PUEDE HACER QUE SE GUARDE TODO JUNTO PORQUE EL PROGRAMA SE PAPEA AL GUARDAR TODO INDIVIDUAL!)
+                    }
+
+                    string strDirectorioZombis = strDirectorioModificado + "\\types_modified_%" + txtPorcentaje.Text + ".xml";
+
+                    XmlDocument docXMLZombis = new XmlDocument();
+
+                    docXMLZombis.Load(strDirectorioZombis);
+
+                    XmlNodeList varListaNodosZombis = docXMLZombis.DocumentElement.SelectNodes("type[starts-with(@name,'ZmbF') or starts-with(@name,'ZmbM')]//min");
+
+                    //Resetea el min de los zombis a 1
+                    foreach (XmlNode varNodoMinZombis in varListaNodosZombis)
+                    {
+                        MessageBox.Show(varNodoMinZombis.InnerText);
+                        string num = "1";
+                        varNodoMinZombis.InnerText = num;
+                        docXMLZombis.Save(strDirectorioModificado + "\\types_modified_%" + txtPorcentaje.Text + ".xml"); //Guarda el min uno a la vez (¡REVISAR ESTO A LO MEJOR SE PUEDE HACER QUE SE GUARDE TODO JUNTO PORQUE EL PROGRAMA SE PAPEA AL GUARDAR TODO INDIVIDUAL!)
+                    }
+
+                    MessageBox.Show("XML replace process DONE!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information); //Mensaje que notifica que el for finalizó
+
+                    lblEstado.Text = "Process finished go to selected final folder";
+                }
+                else
+                {
+                    MessageBox.Show("Percentage field empty, enter a percentage!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //Mensaje en caso de que no ingrese porcentaje
+                }
+            }
+            else
+            {
+                MessageBox.Show("At least one path field empty, select a path!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //Mensaje en caso de que falte un directorio o ambos
+            }
+        }
+
+        private void txtPorcentaje_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Prohibición de la entrada de caracteres en el txtPorcentaje que no sean dígitos o de control
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Only accepts numbers!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDirectorio_Click(object sender, EventArgs e)
+        {
+            //Diálogo para seleccionar la dirección del archivo original funciona parecido al de abrir un frm
+            using (OpenFileDialog OpenFileDialog = new OpenFileDialog())
+            {
+                if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtDirectorioOriginal.Text = OpenFileDialog.FileName;
+                }
+            }
+        }
+
+        private void btnDirectorioModificado_Click(object sender, EventArgs e)
+        {
+            //Diálogo para seleccionar la dirección de la carpeta de guardado funciona parecido al de abrir un frm
+            using (FolderBrowserDialog FolderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (FolderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtDirectorioModificado.Text = FolderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void txtDirectorioOriginal_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDirectorioOriginal.Text != "")
+            {
+                txtDirectorioModificado.Enabled = true;
+                btnDirectorioModificado.Enabled = true;
+                lblDirectorioModificado.Enabled = true;
+                if (txtDirectorioModificado.Text != "")
+                {
+                    lblPorcentaje.Enabled = true;
+                    txtPorcentaje.Enabled = true;
+                    if (txtPorcentaje.Text != "")
+                    {
+                        btnAplicar.Enabled = true;
+                    }
+                }
+            }
+            else
+            {
+                txtDirectorioModificado.Enabled = false;
+                btnDirectorioModificado.Enabled = false;
+                lblDirectorioModificado.Enabled = false;
+                lblPorcentaje.Enabled = false;
+                txtPorcentaje.Enabled = false;
+                btnAplicar.Enabled = false;
+            }
+        }
+
+        private void txtDirectorioModificado_TextChanged(object sender, EventArgs e)
+        {
+            if (txtDirectorioModificado.Text != "")
+            {
+                lblPorcentaje.Enabled = true;
+                txtPorcentaje.Enabled = true;
+                if (txtPorcentaje.Text != "")
+                {
+                    btnAplicar.Enabled = true;
+                }
+            }
+            else
+            {
+                lblPorcentaje.Enabled = false;
+                txtPorcentaje.Enabled = false;
+                btnAplicar.Enabled = false;
+            }
+        }
+
+        private void txtPorcentaje_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPorcentaje.Text != "")
+            {
+                btnAplicar.Enabled = true;
+            }
+            else
+            {
+                btnAplicar.Enabled = false;
+            }
+        }
+
+        private void btnDescargarOriginal_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog FolderBrowserDialog = new FolderBrowserDialog() {Description = "Select the destination folder for the file"})
+            {
+                if (FolderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string strDirectorioTypesOriginal = FolderBrowserDialog.SelectedPath;
+                    File.WriteAllText(strDirectorioTypesOriginal + "\\original_types.xml", Resources.original_types);
+                    MessageBox.Show("File copied to: " + strDirectorioTypesOriginal, "Copied", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strDirectorioOriginal = txtDirectorioOriginal.Text;
+            strDirectorioOriginal = strDirectorioOriginal.Replace(@"\", @"\\");
+
+            XmlDocument docXML = new XmlDocument();
+
+            docXML.Load(strDirectorioOriginal);
+
+            string strDirectorioModificado = txtDirectorioModificado.Text;
+            strDirectorioModificado = strDirectorioModificado.Replace(@"\", @"\\");
+
+
+            XmlNodeList varListaNodos = docXML.DocumentElement.SelectNodes("type[starts-with(@name,'ZmbF')]//min");
+
+            //Resetea el min de los zombis a 1
+            foreach (XmlNode varNodoMinZombis in varListaNodos)
+            {
+                varNodoMinZombis.InnerText = "3"; //Reemplazao del min viejo con el aumentado
+                docXML.Save(strDirectorioModificado + "\\types_modified_ %" + txtPorcentaje.Text + ".xml"); //Guarda el min uno a la vez (¡REVISAR ESTO A LO MEJOR SE PUEDE HACER QUE SE GUARDE TODO JUNTO PORQUE EL PROGRAMA SE PAPEA AL GUARDAR TODO INDIVIDUAL!)
+
+            }
+        }
+    }
+    }
